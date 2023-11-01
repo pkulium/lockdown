7c305eea0f3d5a46a7618b73b7411d6de4cf3312
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-01 13:03:58 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -353,7 +353,7 @@ if __name__ == '__main__':
             for anp_eps in [1.0]:
                 for anp_steps in [1]:
                     for anp_alpha in [0.2]:
-                        for round in [10]:
+                        for round in [5]:
                             local_model, mask_values =  train_mask(-1, global_model, criterion, server_train_loader, mask_lr, anp_eps, anp_steps, anp_alpha, round)
                             print('-' * 64)
                             print(f'|settings: {mask_lr}, {anp_eps}, {anp_steps}, {anp_alpha}, {round} |')
