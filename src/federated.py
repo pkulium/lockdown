b35f211863ad1c3c7cb80cee463db23f2e440b81
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-07 15:58:41 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -22,8 +22,8 @@ import time
 import logging
 from torch.utils.data import DataLoader, ConcatDataset
 
-# SAVE_MODEL_NAME = 'AckRatio4_40_MethodNone_datacifar10_alpha1_Rnd200_Epoch2_inject0.5_dense0.25_Aggavg_se_threshold0.0001_noniidTrue_maskthreshold20_attackbadnet.pt'
-SAVE_MODEL_NAME = 'combined_train.pt'
+SAVE_MODEL_NAME = 'AckRatio4_40_MethodNone_datacifar10_alpha1_Rnd200_Epoch2_inject0.5_dense0.25_Aggavg_se_threshold0.0001_noniidTrue_maskthreshold20_attackbadnet.pt'
+# SAVE_MODEL_NAME = 'combined_train.pt'
 
 
 
