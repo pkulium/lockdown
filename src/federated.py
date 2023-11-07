20b40746c21ad48eb73a07a64e52f2adc24ace20
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-07 16:06:57 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -414,8 +414,8 @@ if __name__ == '__main__':
     # | Attack Loss/Attack Success Ratio: 3.751 / 0.034 |
     pruning_max, pruning_step = 0.95, 0.02
     for _ in range(1):
-        for mask_lr in [0.1]:
-            for anp_eps in [1.0]:
+        for mask_lr in [0.2]:
+            for anp_eps in [0.4]:
                 for anp_steps in [1]:
                     for anp_alpha in [0.2]:
                         for round in [10]:
