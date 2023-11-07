98f856edd5cf90a656b273c70c54a3e6d6c2f485
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-07 08:39:33 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -358,7 +358,7 @@ if __name__ == '__main__':
     if args.rounds > 0:
         exit()
     elif args.rounds < 0:
-        combined_dataset = ConcatDataset([agents[agent_id].train_loader for agent_id in range(args.num_agents)])
+        combined_dataset = ConcatDataset([agent.train_loader.dataset for agent in agents])
         # Create a single DataLoader
         args.combined_train_loader = DataLoader(
             combined_dataset,
