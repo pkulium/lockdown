eba5a037784feb6361fbf1ebbfee4e364b19eac7
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-07 16:17:07 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -403,22 +403,22 @@ if __name__ == '__main__':
     best_val_acc = 0
     best_asr = 1
     id2mask_values = {}
-    # for _ in range(1):
-        # for mask_lr in [0.01, 0.1]:
-            # for anp_eps in [0.4, 0.6, 0.8, 1.0]:
-                # for anp_steps in [1, 5]:
-                #     for anp_alpha in [0.2, 0.4, 0.6]:
-                #         for round in [5, 10]:     
+    pruning_max, pruning_step = 0.95, 0.05
+    for _ in range(1):
+        for mask_lr in [0.01, 0.1]:
+            for anp_eps in [0.4, 0.8]:
+                for anp_steps in [1]:
+                    for anp_alpha in [0.2, 0.6]:
+                        for round in [5, 10]:     
     # |settings: 0.01, 1.0, 1, 0.6, 10 |
     # | Val_Loss/Val_Acc: 1.027 / 0.638 |
     # | Attack Loss/Attack Success Ratio: 3.751 / 0.034 |
-    pruning_max, pruning_step = 0.95, 0.05
-    for _ in range(1):
-        for mask_lr in [0.2]:
-            for anp_eps in [0.4]:
-                for anp_steps in [1]:
-                    for anp_alpha in [0.2]:
-                        for round in [25]:
+    # for _ in range(1):
+    #     for mask_lr in [0.2]:
+    #         for anp_eps in [0.4]:
+    #             for anp_steps in [1]:
+    #                 for anp_alpha in [0.2]:
+    #                     for round in [25]:
                             local_model, mask_values =  train_mask(-1, global_model, criterion, server_train_loader, mask_lr, anp_eps, anp_steps, anp_alpha, round)
                             id2mask_values[-1] = torch.tensor([[mask_values[i][-1] for i in range(len(mask_values)) if i > len(mask_values) //2]])
                             print('-' * 64)
