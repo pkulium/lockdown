830d3948532c15e57e5024c3d3d41c411f20a660
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-08 15:51:27 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -25,7 +25,7 @@ import matplotlib.pyplot as plt
 
 SAVE_MODEL_NAME = 'AckRatio4_40_MethodNone_datacifar10_alpha1_Rnd200_Epoch2_inject0.5_dense0.25_Aggavg_se_threshold0.0001_noniidTrue_maskthreshold20_attackbadnet.pt'
 # SAVE_MODEL_NAME = 'combined_train.pt'
-SAVE_MODEL_NAME = '/work/LAS/wzhang-lab/mingl/code/backdoor/ANP_backdoor/save/model_last.th'
+# SAVE_MODEL_NAME = '/work/LAS/wzhang-lab/mingl/code/backdoor/ANP_backdoor/save/model_last.th'
 
 
 
