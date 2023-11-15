c9edb1d738f27dea65d7e0c272f481cf9a75f1c1
pkulium <54947136+pkulium@users.noreply.github.com>
2023-11-15 09:32:17 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/federated.py

--- src/federated.py
+++ src/federated.py
@@ -457,8 +457,8 @@ if __name__ == '__main__':
     # Best Configuration:  0.1, 1.0, 1, 0.2, 10 
     # Best Configuration Central:  0.2, 0,4, 1, 0.2, 10 
     for _ in range(1):
-        for mask_lr in [0.2]:
-            for anp_eps in [0.4]:
+        for mask_lr in [0.01]:
+            for anp_eps in [1.0]:
                 for anp_steps in [1]:
                     for anp_alpha in [0.2]:
                         for round in [10]:
